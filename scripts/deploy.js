003d0e2de84942b1de1ee7d334b8c2f75aa0626c
santiago restrepo <srest031@fiu.edu>
2023-01-16 23:56:43 -0500

    Deploy Script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,6 +12,33 @@ const tokens = (n) => {
 }
 
 async function main() {
+  // Setup accounts
+  const [deployer] = await ethers.getSigners()
+
+  // Deploy Dappazon
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
+  const dappazon = await Dappazon.deploy()
+  await dappazon.deployed()
+
+  console.log(`Deployed Dappazon Contract at: ${dappazon.address}\n`)
+
+  // Listing items...
+  for(let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).listProducts(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock,
+    )
+
+    await transaction.wait()
+
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+  }
+
 
 }
 
